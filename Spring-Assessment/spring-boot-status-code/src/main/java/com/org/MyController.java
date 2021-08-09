995c9c5b18f4a087e79822235719298e0671ef3b
shinyfelicitav21 <78897934+shinyfelicitav21@users.noreply.github.com>
2021-08-09 15:38:52 +0530

    Add files via upload

---
+++ Spring-Assessment/spring-boot-status-code/src/main/java/com/org/MyController.java
@@ -0,0 +1,25 @@
+package com.org;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+
+    @RequestMapping(value = "/orders/{id}")
+    @ResponseBody
+    public String getOrder(@PathVariable("id") long id) {
+
+        if (id < 0 || id > 500) {
+
+            var message = String.format("Order %d not found", id);
+            throw new OrderNotFoundException(message);
+        }
+
+        var message = String.format("Returning order %d", id);
+
+        return message;
+    }
+}
